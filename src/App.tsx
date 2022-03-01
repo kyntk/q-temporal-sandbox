359f3449d23cb20f9cf607e9611f8eb501e9a04e
kyntk <yuto.kaneko.1128@gmail.com>
2022-03-01 09:33:40 +0900

    Display DateTime

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Temporal } from '@js-temporal/polyfill'
 
 function App() {
+  const now = Temporal.Now
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <span>DateTime:</span>
+        <span>{now.plainDateTimeISO().toLocaleString()}</span>
+      </div>
+      <div>
+        <span>Date:</span>
+        <span>{now.plainDateISO().toLocaleString()}</span>
+      </div>
+      <div>
+        <span>Time:</span>
+        <span>{now.plainTimeISO().toLocaleString()}</span>
+      </div>
+    </>
+  )
 }
-
-export default App;
+export default App
